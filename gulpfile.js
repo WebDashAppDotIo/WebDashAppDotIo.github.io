7b5690c200decd14b4f927ff61b41a170f982e50
Ronan Guilloux <ronan.guilloux@gmail.com>
2015-03-07 22:19:37 +0100

    deps

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,45 @@
 var gulp =          require('gulp'),
+    plugins =       require('gulp-load-plugins')(),
     browserSync =   require('browser-sync'),
     less =          require('gulp-less'),
     jade =          require('gulp-jade'),
     imagemin =      require('gulp-imagemin'),
     uglify =        require('gulp-uglify'),
-    DIST =          './'
+    chalk =         require('chalk'),
+    sourcemaps =    require('gulp-sourcemaps'),
+    source =        require('vinyl-source-stream'),
+    buffer =        require('vinyl-buffer'),
+    DIST =          './',
+    ASSETS =        DIST + '/assets'
     ;
+
+gulp.task('clean', function (cb) {
+    del([
+        ASSETS + '/**'
+    ], cb);
+});
 
 /*
  * Browser-sync task (static server)
  */
 gulp.task('browser-sync', function() {
-    browserSync.init([DIST + '/assets/css/style.css', DIST + '/*.html'], {
+    browserSync.init([ASSETS + '/css/style.css', DIST + '/*.html'], {
         server: {
             baseDir: DIST
         }
     });
 });
 
-/*
- * Less task
- */
-gulp.task('less', function() {
+gulp.task('styles', function() {
     gulp.src('less/style.less')
-        .pipe(less())
-        .pipe(gulp.dest(DIST + '/assets/css'));
+    .pipe(less({compress: true}))
+    .pipe(plugins.minifyCss())
+    .pipe(gulp.dest(ASSETS + '/css'))
+    .pipe(plugins.size({
+        title: 'css'
+    }));
 });
 
-/*
- * Jade task
- */
 gulp.task('templates', function() {
     var texts = {
         'lang': "fr",
@@ -41,74 +51,68 @@ gulp.task('templates', function() {
 
     gulp.src('jades/*.jade')
     .pipe(jade({data: texts}))
-    .pipe(gulp.dest(DIST))
+    .pipe(gulp.dest(DIST));
 });
 
-/*
- * Copy vendors task
- */
 gulp.task('vendors', function() {
     gulp.src('vendors/**/*.*')
-    .pipe(gulp.dest(DIST + '/assets/vendors'));
+    .pipe(gulp.dest(ASSETS + '/vendors'));
 });
 
-/*
- * Copy fonts task
- */
 gulp.task('fonts', function() {
     gulp.src('fonts/**/*.*')
-    .pipe(gulp.dest(DIST + '/assets/fonts'));
+    .pipe(gulp.dest(ASSETS + '/fonts'));
 });
 
-/*
- * Uglify JS
- */
-gulp.task('compress', function() {
+gulp.task('scripts', function() {
     gulp.src('js/*.js')
     .pipe(uglify())
-    .pipe(gulp.dest(DIST + '/assets/js'));
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(ASSETS + '/js'));
+
+    gulp.src('*.js')
+    .pipe(plugins.jshint())
+    .pipe(plugins.jshint.reporter(require('jshint-stylish')));
 });
 
-/*
- * Minify images
- */
 gulp.task('imagemin', function() {
     gulp.src(['img/*.png', 'img/*.jpeg', 'img/*.jpg', 'img/*.gif'])
     .pipe(imagemin())
-    .pipe(gulp.dest(DIST + '/assets/images'));
+    .pipe(gulp.dest(ASSETS + '/images'));
 });
 
-/*
- * Watch tasks
- */
 gulp.task('watch', function() {
-    gulp.watch('less/**/*.less', ['less']);
+    gulp.watch('less/**/*.less', ['styles']);
     gulp.watch('jades/**/*.jade', ['templates']);
     gulp.watch('img/**/*.*', ['imagemin']);
     gulp.watch('vendors/**/*.*', ['vendors']);
-    gulp.watch('js/**/*.*', ['compress']);
+    gulp.watch('js/**/*.*', ['scripts']);
     gulp.watch('fonts/**/*.*', ['fonts']);
+    console.log(chalk.green('Build.complete!'));
 });
 
-/*
- * Default task to be run with `gulp`
- */
 gulp.task('default', [
     'fonts',
     'vendors',
     'imagemin',
-    'less',
-    'compress',
+    //'styles',
+    'scripts',
     'templates',
     'browser-sync',
     'watch'
-]);
+], function(){
+    console.log(chalk.green('Build.complete!'));
+});
 
 gulp.task('build', [
     'fonts',
     'vendors',
     'imagemin',
-    'less',
-    'compress',
+    //'styles',
+    'scripts',
     'templates',
-]);
+], function(){
+    console.log(chalk.green('Build complete!'));
+});
